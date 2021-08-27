514a27faf712f1dc1838cd778add3330286f079c
yetrun <89437145+yetrun@users.noreply.github.com>
2021-08-27 16:33:50 +0800

    添加实现计算属性

---
+++ test/computedAttributes.test.js
@@ -0,0 +1,35 @@
+const test = require('ava')
+const Model = require('../lib/model')
+
+const User = Model.extend({
+  attributes: {
+    firstName: {},
+    lastName: {}
+  },
+  computedAttributes: {
+    fullName: {
+      get () {
+        return this.firstName + ' ' + this.lastName
+      },
+      set (fullName) {
+        const [firstName, lastName] = fullName.split(' ')
+        this.firstName = firstName
+        this.lastName = lastName
+      }
+    }
+  }
+})
+
+test('get fullName', t => {
+  const user = new User({ firstName: 'James', lastName: 'Dean' })
+
+  t.is(user.fullName, 'James Dean')
+})
+
+test('define static action in model class', t => {
+  const user = new User()
+  user.fullName = 'James Dean'
+
+  t.is(user.firstName, 'James')
+  t.is(user.lastName, 'Dean')
+})
