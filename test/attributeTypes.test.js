a47fd79783c6938060e04373cbb158d2bdfe615f
yetrun <89437145+yetrun@users.noreply.github.com>
2021-09-02 09:46:13 +0800

    类型转化时确保不转化 undefined 和 null

--- test/attributeTypes.test.js
+++ test/attributeTypes.test.js
@@ -8,6 +8,17 @@ const User = Model.extend({
   }
 })
 
+const TypesModel = Model.extend({
+  attributes: {
+    aString: { type: String },
+    aNumber: { type: Number },
+    aBoolean: { type: Boolean },
+    aDate: { type: Date },
+    anArray: { type: Array },
+    anObject: { type: Object }
+  }
+})
+
 test('string is not number', t => {
   t.not('18', 18)
 })
@@ -34,4 +45,38 @@ test('convert types in setter', t => {
 
   t.is(user.name, 'Jim')
   t.is(user.age, 18)
+})
+
+test('not convert undefined', t => {
+  const model = new TypesModel({ 
+    aString: undefined,
+    aNumber: undefined,
+    aBoolean: undefined,
+    aDate: undefined,
+    anArray: undefined,
+    anObject: undefined,
+  })
+  t.is(model.aString, undefined)
+  t.is(model.aNumber, undefined)
+  t.is(model.aBoolean, undefined)
+  t.is(model.aDate, undefined)
+  t.is(model.anArray, undefined)
+  t.is(model.anObject, undefined)
+})
+
+test('not convert null', t => {
+  const model = new TypesModel({ 
+    aString: null,
+    aNumber: null,
+    aBoolean: null,
+    aDate: null,
+    anArray: null,
+    anObject: null,
+  })
+  t.is(model.aString, null)
+  t.is(model.aNumber, null)
+  t.is(model.aBoolean, null)
+  t.is(model.aDate, null)
+  t.is(model.anArray, null)
+  t.is(model.anObject, null)
 })
