c5302f11120a74b378b3ee5517a2d68d09fdcc3d
yetrun <run27017@126.com>
2021-10-07 19:38:56 +0800

    action 自动调用 transform 方法，并添加普通的 methods 块

--- test/transform.test.js
+++ test/transform.test.js
@@ -12,7 +12,7 @@ test('derive', t => {
   t.deepEqual(user2.attributes, { name: 'Jim', age: 18 })
 })
 
-test('transform 方法会将属性传递给上游', t => {
+test('默认情况下，transform 方法会将属性传递给上游', t => {
   const User = Model.extend({ 
     attributes: { name: {}, age: {} },
   })
@@ -42,7 +42,23 @@ test('自定义 onTransform', t => {
   t.deepEqual(user.attributes2, { name: 'James', age: 19 })
 })
 
-test('调用 transform 方法对新建的 record 没有影响', t => {
+test('action 会默认调用 transform', t => {
+  const User = Model.extend({
+    attributes: { name: {}, age: {} },
+    actions: {
+      save () {}
+    }
+  })
+
+  const user = new User({ name: 'Jim', age: 18 })
+  const user2 = user.derive()
+
+  user2.attributes = { name: 'James', age: 19 }
+  user2.save()
+  t.deepEqual(user.attributes, { name: 'James', age: 19 })
+})
+
+test('调用 transform 方法对没有连接的 record 没有影响', t => {
   const User = Model.extend({
     attributes: { name: {}, age: {} },
     onTransform (attributes) {
