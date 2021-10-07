4761482e19285a024ba771e95eca85f6340dc8a6
yetrun <run27017@126.com>
2021-10-07 19:38:56 +0800

    action 自动调用 transform 方法，并添加普通的 methods 块

--- test/methods.test.js
+++ test/methods.test.js
@@ -9,6 +9,16 @@ const User = Model.extend({
   actions: {
     foo () {
       return this.attributes
+    },
+    asyncFoo () {
+      return new Promise(resolve => {
+        setTimeout(() => resolve('foo'), 0)
+      })
+    }
+  },
+  methods: {
+    bar () {
+      return 'bar'
     }
   },
   static: {
@@ -23,6 +33,20 @@ test('define action in prototype', t => {
 
   t.true('foo' in Object.getPrototypeOf(user))
   t.deepEqual(user.foo(), user.attributes)
+})
+
+test('define async action in prototype', async t => {
+  const user = new User({ name: 'Jim', age: 18 })
+
+  t.true('asyncFoo' in Object.getPrototypeOf(user))
+  t.deepEqual(await user.asyncFoo(), 'foo')
+})
+
+test('define method in prototype', t => {
+  const user = new User({ name: 'Jim', age: 18 })
+
+  t.true('bar' in Object.getPrototypeOf(user))
+  t.deepEqual(user.bar(), 'bar')
 })
 
 test('define static action in model class', t => {
