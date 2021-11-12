458af8231f98a2c622b6ca320922e83247faabdb
yetrun <89437145+yetrun@users.noreply.github.com>
2021-11-12 19:10:40 +0800

    为 Model 基类定义一个静态块 defineAttributes

---
+++ test/inherit-style/modelDefineAttributes.test.js
@@ -0,0 +1,22 @@
+const test = require('ava')
+const Model = require('../../lib/model')
+
+class User extends Model {
+  static defineAttributes () {
+    this.defineAttribute('name', {})
+    this.defineAttribute('age', {})
+  }
+}
+
+test('测试 defineAttributes 成功设置了属性定义', t => {
+  const user = new User()
+  user.attributes = {
+    name: 'Jim',
+    age: 18,
+    foo: 'foo'
+  }
+
+  t.is(user.name, 'Jim')
+  t.is(user.age, 18)
+  t.is(user.foo, undefined)
+})
